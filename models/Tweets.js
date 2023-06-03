18f59351a2ff50982ad1263b3454b26314231714
OlexiiA <incretiblealex@gmail.com>
2023-06-03 21:27:24 +0300

    fix json

--- models/Tweets.js
+++ models/Tweets.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const TweetSchema = new mongoose.Schema(
   {
     email: { type: String },
-    title: { type: String, required: true },
-    text: { type: String, required: true },
+    title: { type: String, required: true, maxlength: 20 },
+    text: { type: String, required: true, maxlength: 280 },
     imageUrl: { type: String, default: "" }, 
     views: { type: Number, default: 0 },
     author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
